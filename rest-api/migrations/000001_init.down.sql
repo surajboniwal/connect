657293c37465ade0225fbfa45fce60ffe81dbd17
surajboniwal <surajboniwal18@gmail.com>
2023-07-23 07:50:24 +0000

    db schema

--- rest-api/migrations/000001_init.down.sql
+++ rest-api/migrations/000001_init.down.sql
@@ -1,7 +1,32 @@
-DROP INDEX IF EXISTS "email_unique";
-DROP INDEX IF EXISTS "phone_unique";
-DROP INDEX IF EXISTS "organization_user_unique";
+-- Drop foreign keys
+ALTER TABLE "organizations" DROP CONSTRAINT "organizations_id_fkey";
+ALTER TABLE "organization_users" DROP CONSTRAINT "organization_users_organization_id_fkey";
+ALTER TABLE "organization_users" DROP CONSTRAINT "organization_users_user_id_fkey";
+ALTER TABLE "organization_channels" DROP CONSTRAINT "organization_channels_organization_id_fkey";
+ALTER TABLE "organization_channels" DROP CONSTRAINT "organization_channels_channel_id_fkey";
+ALTER TABLE "customer_channels" DROP CONSTRAINT "customer_channels_channel_id_fkey";
+ALTER TABLE "customer_channels" DROP CONSTRAINT "customer_channels_customer_id_fkey";
 
+-- Drop triggers
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "users";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "organizations";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "organization_users";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "customers";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "channels";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "organization_channels";
+DROP TRIGGER IF EXISTS trigger_update_timestamp ON "customer_channels";
+
+-- Drop indexes
+DROP INDEX IF EXISTS "customer_channel_unique";
+DROP INDEX IF EXISTS "organization_user_unique";
+DROP INDEX IF EXISTS "phone_unique";
+DROP INDEX IF EXISTS "email_unique";
+
+-- Drop tables
+DROP TABLE IF EXISTS "customer_channels";
+DROP TABLE IF EXISTS "organization_channels";
+DROP TABLE IF EXISTS "channels";
+DROP TABLE IF EXISTS "customers";
 DROP TABLE IF EXISTS "organization_users";
-DROP TABLE IF EXISTS "users";
 DROP TABLE IF EXISTS "organizations";
+DROP TABLE IF EXISTS "users";
--- rest-api/migrations/000001_init.up.sql
+++ rest-api/migrations/000001_init.up.sql
@@ -4,34 +4,73 @@ CREATE TABLE "users" (
   "email" text NOT NULL,
   "password" text NOT NULL,
   "phone" varchar(15) NOT NULL,
-  "created_at" timestamp default CURRENT_TIMESTAMP NOT NULL,
-  "updated_at" timestamp default CURRENT_TIMESTAMP NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
   PRIMARY KEY ("id")
 );
 
 CREATE TABLE "organizations" (
   "id" bigint,
   "name" text NOT NULL,
-  "created_at" timestamp default CURRENT_TIMESTAMP NOT NULL,
-  "updated_at" timestamp default CURRENT_TIMESTAMP NOT NULL,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now()),
   PRIMARY KEY ("id")
 );
 
 CREATE TABLE "organization_users" (
   "organization_id" bigint NOT NULL,
   "user_id" bigint NOT NULL,
-  "created_at" timestamp default CURRENT_TIMESTAMP NOT NULL,
-  "updated_at" timestamp default CURRENT_TIMESTAMP NOT NULL
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "customers" (
+  "id" bigint,
+  "name" text NOT NULL,
+  "organization_id" bigint,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "channels" (
+  "id" bigint,
+  "name" text NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "organization_channels" (
+  "id" bigint,
+  "organization_id" bigint,
+  "channel_id" bigint,
+  "metadata" jsonb,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now()),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "customer_channels" (
+  "customer_id" bigint,
+  "channel_id" bigint,
+  "metadata" jsonb,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE UNIQUE INDEX "email_unique" ON "users" ("email");
 CREATE UNIQUE INDEX "phone_unique" ON "users" ("phone");
 CREATE UNIQUE INDEX "organization_user_unique" ON "organization_users" ("organization_id", "user_id");
+CREATE UNIQUE INDEX "customer_channel_unique" ON "customer_channels" ("customer_id", "channel_id");
 
-ALTER TABLE "organization_users" ADD FOREIGN KEY ("organization_id") REFERENCES "organizations" ("id") ON DELETE CASCADE;
-ALTER TABLE "organization_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
-
-
+ALTER TABLE "customer_channels" ADD FOREIGN KEY ("channel_id") REFERENCES "channels" ("id");
+ALTER TABLE "customer_channels" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("id");
+ALTER TABLE "organization_channels" ADD FOREIGN KEY ("organization_id") REFERENCES "organizations" ("id") ON DELETE NO ACTION;
+ALTER TABLE "organization_channels" ADD FOREIGN KEY ("channel_id") REFERENCES "channels" ("id") ON DELETE NO ACTION;
+ALTER TABLE "organization_users" ADD FOREIGN KEY ("organization_id") REFERENCES "organizations" ("id") ON DELETE NO ACTION;
+ALTER TABLE "organization_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE NO ACTION;
+ALTER TABLE "customers" ADD FOREIGN KEY ("organization_id") REFERENCES "organizations" ("id");
 
 
 
